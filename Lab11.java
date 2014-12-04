2cb4a71edcffe9caf494310309db589fcb47ee6c
Kent Collins <kent.r.collins@gmail.com>
2014-12-04 10:07:54 +0000

    Update and rename Lab10.java to Lab11.java

---
+++ Lab11.java
@@ -0,0 +1,17 @@
+/**
+ * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
+ * 
+ * @author <...>
+ * @version <...>
+ *
+ */
+
+import edu.fcps.karel2.Display; 
+
+public class Lab11 {
+	 
+	 public static void main(String[] args) {
+	
+	 }
+	 
+ }
