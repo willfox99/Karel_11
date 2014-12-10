c0135530a54ba4a11a598fea28ada842d61c9d74
willfox99 <william_fox@asl.org>
2014-12-10 11:34:58 +0000

    Finished

--- Lab11.java
+++ Lab11.java
@@ -1,16 +1,37 @@
 /**
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
- * @author <...>
- * @version <...>
+ * @author <Will Fox>
+ * @version <>
  *
  */
 
-import edu.fcps.karel2.Display; 
+import edu.fcps.karel2.Display;
+import edu.fcps.Digit;  
 
 public class Lab11 {
 	 
 	 public static void main(String[] args) {
+    
+        Display.openWorld("maps/.map");
+	 Display.setSize(36, 32);
+	 Display.setSpeed(10);
+    
+    
+    Digit first = new Zero(7,9);
+    //Digit second = new Six(7,9);
+    Digit third = new Four(1,9);
+    Digit fourth = new Four(13,9);
+    //Digit fifth = new Two(25,9);
+    //Digit sixth = new sixth(31,9);
+    
+    first.display();
+    //second.display();
+    third.display();
+    fourth.display();
+    //fifth.display();
+    //sixth.display();
+
 	
 	 }
 	 
